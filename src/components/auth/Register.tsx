0d2e7b40f38d1761c87d3f8e6d885e38a1fb0bae
JLJL <juanluisjaramillolopez@gmail.com>
2024-09-24 11:05:05 -0600

    creacion de tipos de datos a resivir y creacion del componente de registro

--- src/components/auth/Register.tsx
+++ src/components/auth/Register.tsx
@@ -1,7 +1,84 @@
+import { Box, TextField, Button } from '@mui/material'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { Auth } from '../../interfaces/globalTypes'
 
 const Register = () => {
+    //usaremos la libreria de react-hook-form para registrar los datos del usuario
+    const { handleSubmit, reset, register, formState: { errors } } = useForm<Auth>()//le pasamos los parametros a esperar
+
+    //creamos la funcion de envio de datos del usuario
+    const onSubmit: SubmitHandler<Auth> = (data) => {
+        try {
+            console.log(data)
+            reset()
+        } catch (error) {
+            if(error instanceof Error) {
+                console.log(error.message)
+            }
+        }
+    }
   return (
-    <div>Register</div>
+    <Box component='form' width='70%' m='0 auto' onSubmit={ handleSubmit(onSubmit) }>
+        <TextField
+          id="username"
+          label="username"
+          margin='normal'
+          type='text'
+          error={ !!errors?.username }
+          helperText={ errors?.username?.message }
+          fullWidth
+          {
+            ...register('username', {
+                required: {
+                    value: true,
+                    message: 'Username is required'
+                }
+            })
+          }
+        />
+
+<TextField
+          id="email"
+          label="email"
+          margin='normal'
+          type='email'
+          error={ !!errors?.email }
+          helperText={ errors?.email?.message }
+          fullWidth
+          {
+            ...register('email', {
+                required: {
+                    value: true,
+                    message: 'Email is required'
+                }
+            })
+          }
+        />
+
+<TextField
+          id="password"
+          label="password"
+          margin='normal'
+          type='password'
+          error={ !!errors?.password }
+          helperText={ errors?.password?.message }
+          fullWidth
+          {
+            ...register('password', {
+                required: {
+                    value: true,
+                    message: 'Password is required'
+                }
+            })
+          }
+        />
+
+        <Box display='flex' justifyContent='center'>
+            <Button variant="contained" color="secondary" type='submit'>
+              Register User
+            </Button>
+        </Box>
+    </Box>
   )
 }
 
---
+++ src/interfaces/globalTypes.ts
@@ -0,0 +1,63 @@
+export interface Auth{
+    username?: string;
+    email: string;
+    password: string;
+}
+
+export interface UserFlotilla{
+    id_users: string;
+    name: string;
+    email: string;
+    password: string;
+    role: string;
+    phone_number: number;
+}
+
+export interface Divers{
+    id_drivers: string;
+    user_id: string;
+    licence_number: number;
+    license_type: string;
+    experience_year: number;
+    vehicle_id: string;
+}
+
+export interface Trips{
+    id_trip: string;
+    vehicle_id: string;
+    driver_id: string;
+    transport_id: string;
+    origin: string;
+    destination: string;
+    distance: number;
+    trip_date: string;
+    status: string;
+    fuel_consumed: number;
+    notes: string;
+}
+
+export interface Vehicles{
+    id_vehicle: string;
+    plate_number: string;
+    model: string;
+    brand: string;
+    type: string;
+    capcity: number;
+    state: string;
+    mileage: number;
+}
+
+export interface Transport{
+    id_transport: string;
+    name: string;
+    description: string;
+}
+
+export interface Maintenance{
+    id_maintenance: string;
+    vehicle_id: string;
+    maintenance_type: string;
+    date: string;
+    cost: number;
+    notes: string;
+}
