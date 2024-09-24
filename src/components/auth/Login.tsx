531488e27c789fb145c59903bb4a05b989a29f18
JLJL <juanluisjaramillolopez@gmail.com>
2024-09-24 11:20:58 -0600

    creacion del login

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,8 +1,73 @@
+import { Box, Button, TextField } from "@mui/material";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Auth } from "../../interfaces/globalTypes";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  //usaremos la libreria de react-hook-form para registrar los datos del usuario
+  const {
+    handleSubmit,
+    reset,
+    register,
+    formState: { errors },
+  } = useForm<Auth>(); //le pasamos los parametros a esperar
 
-export default Login
+  //creamos la funcion de envio de datos del usuario
+  const onSubmit: SubmitHandler<Auth> = (data) => {
+    try {
+      console.log(data);
+      reset();
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error.message);
+      }
+    }
+  };
+  return (
+    <Box
+      component="form"
+      width="70%"
+      m="0 auto"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <TextField
+        id="email"
+        label="email"
+        margin="normal"
+        type="email"
+        error={!!errors?.email}
+        helperText={errors?.email?.message}
+        fullWidth
+        {...register("email", {
+          required: {
+            value: true,
+            message: "Email is required",
+          },
+        })}
+      />
+
+      <TextField
+        id="password"
+        label="password"
+        margin="normal"
+        type="password"
+        error={!!errors?.password}
+        helperText={errors?.password?.message}
+        fullWidth
+        {...register("password", {
+          required: {
+            value: true,
+            message: "Password is required",
+          },
+        })}
+      />
+
+      <Box display="flex" justifyContent="center">
+        <Button variant="contained" color="secondary" type="submit">
+          Login
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default Login;
