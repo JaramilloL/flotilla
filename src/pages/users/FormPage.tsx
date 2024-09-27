0b1696bc49b444251ef1632d0893b41ac9fac592
JLJL <juanluisjaramillolopez@gmail.com>
2024-09-27 16:18:07 -0600

    validacion del formulario para el acceso a agregar datos

--- src/pages/users/FormPage.tsx
+++ src/pages/users/FormPage.tsx
@@ -1,6 +1,20 @@
+import { useContext } from "react"
+import { UserContext } from "../../context/UserContext"
+import { Navigate } from "react-router-dom"
 import CreateUser from "../../components/users/CreateUser"
 
 const FormPage = () => {
+    //traemos el contexto para verificar si existe el login de usuario
+    const context =  useContext(UserContext)
+
+    if(!context) {
+        throw new Error("no context")
+    }
+
+    const { user } = context;
+
+    if(!user) return <Navigate to='/'/>
+
   return (
     <div>
         <CreateUser/>
