5123e4a614ae78925a524bb093fb218bc8e1831f
JLJL <juanluisjaramillolopez@gmail.com>
2024-10-09 13:13:50 -0600

    creamos el contexto de si existe el usuario pues este tenga o no acceso a ver el contenido de transportes

--- src/components/transport_types/TransportInfo.tsx
+++ src/components/transport_types/TransportInfo.tsx
@@ -1,8 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import { Transport } from "../../interfaces/globalTypes";
 import TrasportList from "./TrasportList";
 import { Typography } from "@mui/material";
+import { UserContext } from "../../context/UserContext";
+import { Navigate } from "react-router-dom";
 
 
 //creamos el acceso a la base de datos de usuer para mostrar informacion
@@ -33,6 +35,18 @@ const TransportInfo = () => {
             }
         }
     },[])
+
+    //graemos el contexto dela app para ver si el usuario esta autenticado
+    const context = useContext(UserContext);
+
+    if(!context){
+        throw new Error('no context available')
+    }
+
+    const { user, loadingAuth } = context || {};
+
+    if(loadingAuth) return <h1>Loading...</h1>
+    if(!user) return <Navigate to='/'/>
   return (
     <div>
         <Typography variant="h5" textAlign='center'>Types of Trasnports</Typography>
